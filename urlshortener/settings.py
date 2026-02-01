48585ceb1f99e93f9757b3c10cc789260c12772d
rohan007-suii <rohan123cr777@gmail.com>
2026-02-01 14:10:00 +0545

    Add Render URL to ALLOWED_HOSTS

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-dev-key-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["vrit-task-1.onrender.com"]
 
 
 # Application definition
